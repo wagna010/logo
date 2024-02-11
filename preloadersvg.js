a1276beb6f9a5f6e928a9a74c2a6af450df34ad0
wagna010 <56608142+wagna010@users.noreply.github.com>
2024-02-11 13:26:17 -0300

    Update preloadersvg.js

--- preloadersvg.js
+++ preloadersvg.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://raw.githubusercontent.com/wagna010/logo/main/loalding.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
