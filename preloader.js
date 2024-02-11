00507aac1b753c5fc3c0017ad22640187600840f
wagna010 <56608142+wagna010@users.noreply.github.com>
2024-02-10 22:52:43 -0300

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,49 @@
+const overlay = document.createElement('div');
+overlay.id = 'loader';
+overlay.style.cssText = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 999;
+`;
+
+const lottieContainer = document.createElement('div');
+lottieContainer.id = 'lottieContainer';
+lottieContainer.style.cssText = `
+    max-width: 100%;
+    max-height: 100%;
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+lottieContainer.style.display = 'none';
+
+document.body.appendChild(overlay);
+document.body.appendChild(lottieContainer);
+
+function hideOverlay() {
+    overlay.style.display = 'none';
+    lottieContainer.style.display = 'block';
+    // Substitua pela URL direta do seu arquivo JSON do Lottie.
+    lottie.loadAnimation({
+        container: lottieContainer,
+        renderer: 'svg', // ou 'canvas' se preferir
+        loop: true,
+        autoplay: true,
+        path: 'https://wagna010.github.io/preloaderJSLottie/Tocai.json',
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    hideOverlay(); // Teste sem o setTimeout
+});
+
+window.addEventListener('load', hideOverlay);
