3dcb223afc61f06017b9a96d1c8448ac675f46df
wagna010 <56608142+wagna010@users.noreply.github.com>
2024-02-11 13:31:21 -0300

    Update preloadersvg.js

--- preloadersvg.js
+++ preloadersvg.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://raw.githubusercontent.com/wagna010/logo/main/loalding.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/preload.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
